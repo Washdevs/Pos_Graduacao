36891f785f93abb547e928d8d0d4a9232ce9d9d0
washington <lepexgamer102000@gmail.com>
2025-04-17 17:41:12 -0300

    explic

--- Trilha_Back_com_Express_NodeJS/Exercicios/arrow.js
+++ Trilha_Back_com_Express_NodeJS/Exercicios/arrow.js
@@ -1,3 +1,17 @@
 // Apresentando Arrow Function
-
 // ( arg1, arg2, ..., argn ) => { /* Faça coisas de função aqui */ }
+
+
+
+
+// A sintaxe da função de seta também pode variar, dependendo de vários fatores. 
+// A sintaxe pode variar ligeiramente dependendo do número de argumentos passados ​​para a função e do número de linhas de código no corpo da função.
+// As condições especiais de sintaxe são resumidas na lista a seguir:
+
+// Argumento de entrada único
+// Nenhum argumento de entrada
+// Corpo de função de linha única
+// Expressão única dividida em várias linhas
+// Valor de retorno literal do objeto
+
+
